43685e97ea6f8df7e437fcbb390dae1f42b3b322
towelie03 <dbalsara2003@gmail.com>
2025-11-14 10:10:16 -0800

    hyprland monitors

--- apps/hypr/binds.nix
+++ apps/hypr/binds.nix
@@ -1,5 +1,5 @@
 {
-    "$mainMod" = "SUPER";
+    "$mod" = "SUPER";
 
     bind =
       [
@@ -46,13 +46,16 @@
         "SUPERCTRL, Equal, splitratio, 0.1"
       ]
       ++ (
+        # workspaces
+        # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
         builtins.concatLists (builtins.genList (i:
-          let ws = i + 1;
-          in [
-            "$mainMod, ${toString ws}, workspace, ${toString ws}"
-            "$mainMod SHIFT, ${toString ws}, movetoworkspace, ${toString ws}"
-          ]
-        ) 10)
+            let ws = i + 1;
+            in [
+              "$mod, code:1${toString i}, workspace, ${toString ws}"
+              "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+            ]
+          )
+          9)
       );
 
     bindle = [
--- apps/hypr/rules.nix
+++ apps/hypr/rules.nix
@@ -1,27 +1,26 @@
 {
     workspace = [
-      "1, monitor:DP-1"
-      "2, monitor:DP-1"
-      "3, monitor:DP-1"
-      "4, monitor:DP-1"
-      "5, monitor:DP-1"
+      "1, monitor:DP-4"
+      "2, monitor:DP-4"
+      "3, monitor:DP-4"
+      "4, monitor:DP-4"
+      "5, monitor:DP-4"
 
-      "6, monitor:DP-2"
-      "7, monitor:DP-2"
-      "8, monitor:DP-2"
-      "9, monitor:DP-2"
-      "10, monitor:DP-2"
+      "6, monitor:DP-5"
+      "7, monitor:DP-5"
+      "8, monitor:DP-5"
+      "9, monitor:DP-5"
+      "10, monitor:DP-5"
     ];
 
     windowrule = [
       "workspace 9 silent, class:^(steam)$"
       "workspace 7 silent, class:^(feishin)$"
       "workspace 6 silent, class:^(discord)$"
-      "workspace 10 silent, class:^(keepassxc)$"
       "workspace 9, class:^(mpv)$"
 
-      "float, class:^(nautilis)$"
-      "size 875 550, class:^(nautilis)$"
+      "float, class:^(nautilus)$"
+      "size 875 550, class:^(nautilus)$"
       "float, class:^(mpv)$"
 
       # opacity rules
