168548de60c6201620bd6c4a6d5242252ecbf2d7
towelie03 <dbalsara2003@gmail.com>
2026-02-06 17:57:05 -0800

    for hypr

--- apps/dms/dms-shell.nix
+++ apps/dms/dms-shell.nix
@@ -6,13 +6,36 @@
       enable = true;           # Systemd service for auto-start
       restartIfChanged = true; # Auto-restart dms.service when dankMaterialShell changes
     };
+    
 
+    managePluginSettings = true;
+    
     # Core features
     enableSystemMonitoring = true;   # System monitoring widgets (dgop)
     enableVPN = true;                # VPN management widget
     enableDynamicTheming = true;     # Wallpaper-based theming (matugen)
     enableAudioWavelength = true;    # Audio visualizer (cava)
     enableCalendarEvents = true;     # Calendar integration (khal)
+    
+    plugins = {
+      # Simply enable plugins by their ID (from the registry)
+      dockerManager.enable = true;
+      dankKDEConnect.enable = true;
+      dankLauncherKeys.enable = true;
+      tailscale.enable = true;
+      nixMonitor.enable = true;
+      
+      # Add plugin-specific settings
+      mediaPlayer = {
+        enable = true;
+
+        # You can only define settings here if using the home-manager module
+        settings = {
+          preferredSource = "feishin";
+        };
+      };
+    };
+
   };
 }
 
