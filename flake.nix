93cab3b103b7d8296a3d95d644a8afd26fb2441c
towelie03 <dbalsara2003@gmail.com>
2026-02-14 13:01:46 -0800

    ollama

--- flake.nix
+++ flake.nix
@@ -34,7 +34,7 @@
     };
 
     dms = {
-      url = "github:AvengeMedia/DankMaterialShell";
+      url = "github:AvengeMedia/DankMaterialShell/stable";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
--- pkgs/Megatronus/packages.nix
+++ pkgs/Megatronus/packages.nix
@@ -24,6 +24,7 @@
     nixfmt
     black
     satty
+    ollama-cuda
 
     # Utilities
     jq
