110d0720719b287884e6fd220aad5942dfebecfe
towelie03 <dbalsara2003@gmail.com>
2026-04-01 21:55:01 -0700

    push

--- hosts/Megatronus/configuration.nix
+++ hosts/Megatronus/configuration.nix
@@ -102,7 +102,7 @@
         efiSupport = true;
         device = "nodev";
         # efiInstallAsRemovable = true; # uncomment if firmware ignores variables
-        useOSProber = true;
+        boot.loader.grub.useOSProber = true;
       };
     };
 
@@ -177,7 +177,7 @@
     enable = true;
     enable32Bit = true;
     extraPackages = with pkgs; [
-      config.boot.kernelPackages.nvidiaPackages.beta
+      config.boot.kernelPackages.nvidiaPackages.beta 
       nvidia-vaapi-driver
     ];
   };
